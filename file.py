c814c9c46c27517e2f7cea8f4a691d9053229ec8
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2021-06-08 11:13:01 +0530

    Delete file.py

--- file.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-from youtube_transcript_api import YouTubeTranscriptApi
-import pickle
-
-summarizer = pipeline('summarization')
-pkl_file= open('file.pkl', 'wb')
-pickle.dump(summarizer,pkl_file)
-pkl_file.close()
